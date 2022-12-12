74c2e44e7bad7b1509d7dfad14a778c29a97723d
Nahid hassan <nahidlikhon22@gmail.com>
2022-12-12 21:19:45 +0600

    emailjs added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-
+import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-
 import { toast } from "react-hot-toast";
 
 
 AOS.init();
 const Contact = () => {
+     const form = useRef();
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        emailjs
-        .sendForm(
-          "service_j36j37e",
-          "template_k8k1nue",
-           event.target,
-          "0D_-puATn7oW6obdq"
-        )
-        .then(
-          (result) => {
-            toast.success(result.text);
-          },
-          (error) => {
-            toast.error(error.text);
-          }
-        );
-        
+        emailjs.sendForm('service_j4i17rf', 'template_emk37zn', form.current, 'PKvCuvjLU6GGO6WNZ')
+        .then((result) => {
+            toast('Your message sent')
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
 
+      
         event.target.reset();
       };
       
@@ -37,6 +29,7 @@ const Contact = () => {
     <div id="contact" data-aos="zoom-in">
       <h1 className="my-5 text-5xl text-center">Send Email</h1>
       <form
+       ref={form}
         className="flex flex-col p-5 mx-auto mb-10 border rounded-md shadow-2xl lg:w-1/2 border-spacing-1 border-cyan-400"
         onSubmit={handleSubmit}
         
@@ -44,24 +37,24 @@ const Contact = () => {
         <input
           type="text"
           className="w-full px-5 my-5 text-black border-none shadow-md input bg-none"
-          name="name"
+          name="user_name"
           placeholder="Full Name"
         />
         <input
           className="w-full px-5 my-5 text-black input"
           type="email"
-          name="email"
+          name="user_email"
           placeholder="Your Email"
         />
         <input
           type="text"
           className="w-full px-5 my-5 text-black border-none shadow-md input bg-none"
-          name="subject"
+          name="user_subject"
           placeholder="Subject"
         />
         <textarea
           className="w-full h-32 p-5 my-5 text-black input"
-          name="text"
+          name="message"
           placeholder="Write your email..."
         />
 
